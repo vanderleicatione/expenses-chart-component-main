8bf86753e254e015f9435e806fca5a31f6ea3ad7
Vanderlei Catione Junior <vanderleicatione32@gmail.com>
2023-02-03 15:35:29 -0300

    get chart json from path

--- chart.js
+++ chart.js
@@ -4,37 +4,7 @@ async function getData(path) {
   return data;
 }
 
-var response = [
-  {
-    day: "mon",
-    amount: 17.45,
-  },
-  {
-    day: "tue",
-    amount: 34.91,
-  },
-  {
-    day: "wed",
-    amount: 52.36,
-  },
-  {
-    day: "thu",
-    amount: 31.07,
-  },
-  {
-    day: "fri",
-    amount: 23.39,
-  },
-  {
-    day: "sat",
-    amount: 43.28,
-  },
-  {
-    day: "sun",
-    amount: 25.48,
-  },
-];
-
+var response = await getData('data.json');
 var object = response.reduce(
   (object, item) => ((object[item.day] = item.amount), object),
   {}
